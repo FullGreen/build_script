6d30bd90f91890844528d3b669b467b7c43d20a6
풀그린(Fullgreen) <abin030187@naver.com>
2016-07-29 10:51:42 +0900

    Version 1.2.4

--- c1skt.sh
+++ c1skt.sh
@@ -35,7 +35,7 @@ device=c1skt #기기명
 #ROM Source Download                                                        
 #######################################################################
 echo "───────────────────────────────────────────────────" 
-echo "         Fullgreen BUILD Script[1.2.3.G]│$device   "
+echo "         Fullgreen BUILD Script[1.2.4]│$device   "
 echo "───────────────────────────────────────────────────" 
 echo "cy  │ Cyanogenmod"
 echo "cyos│ CyanogenOS"
@@ -48,7 +48,7 @@ echo "cr  │ CrdroidAndroid"
 echo "na  │ Namelessrom"
 echo "xo  │ XOSP"
 echo "ha  │ Haxynox"
-echo "om  │ Omni[LP]"
+echo "om  │ Omnirom"
 
 if [ -a cy ]; then
 patch=cy && buildprop=cy
@@ -127,7 +127,7 @@ ha)
                   patch=ha && buildprop=ha
 ;;
 om)
-					repo init -u git://github.com/omnirom/android.git -b android-5.1 && touch om
+					repo init -u git://github.com/omnirom/android.git -b android-6.0 && touch om
                   patch=om && buildprop=om
 ;;
 esac
@@ -344,10 +344,13 @@ cm=n
 om)
 echo "Vendor의 apns-conf파일 삭제"
 rm -f vendor/omni/prebuilt/etc/apns-conf.xml
-echo "" > vendor/omni/config/cdma.mk
-echo "# SIM Toolkit
+echo "PRODUCT_COPY_FILES += \
+    vendor/omni/prebuilt/etc/selective-spn-conf.xml:system/etc/selective-spn-conf.xml
+
+# SIM Toolkit
 PRODUCT_PACKAGES += \
-Stk" > vendor/omni/config/gsm.mk
+    Stk
+" > vendor/omni/config/gsm.mk
 cm=n
 ;;
 esac
@@ -633,28 +636,28 @@ git clone https://github.com/CyanogenMod/android_external_guava.git -b cm-13.0 e
 ;;
 
 om)
-if [ -d device/samsung/i9300 ]; then 
+if [ -d device/samsung/c1skt ]; then 
 tput setaf 1
-echo "device/samsung/i9300[PASS]"
+echo "device/samsung/c1skt[PASS]"
 tput setaf 3
-cd device/samsung/i9300 && git pull && cd .. && cd .. && cd ..
+cd device/samsung/c1skt && git pull && cd .. && cd .. && cd ..
 else
 tput setaf 2
-echo "device/samsung/i9300[DOWNLOAD]"
+echo "device/samsung/c1skt[DOWNLOAD]"
 tput setaf 3
-git clone https://github.com/Fullgreen/omnirom_device_samsung_i9300.git -b android-5.1 device/samsung/i9300
+git clone https://github.com/FullgreenDEVomnirom/omnirom_device_samsung_c1skt.git -b android-6.0 device/samsung/c1skt
 fi
 
-if [ -d device/samsung/smdk4412-common ]; then 
+if [ -d device/samsung/c1skt-common ]; then 
 tput setaf 1
-echo "device/samsung/smdk4412-common[PASS]"
+echo "device/samsung/c1skt-common[PASS]"
 tput setaf 3
-cd device/samsung/smdk4412-common && git pull && cd .. && cd .. && cd ..
+cd device/samsung/c1skt-common && git pull && cd .. && cd .. && cd ..
 else
 tput setaf 2
-echo "device/samsung/smdk4412-common[DOWNLOAD]"
+echo "device/samsung/c1skt-common[DOWNLOAD]"
 tput setaf 3
-git clone https://github.com/FullGreen/omnirom_device_samsung_smdk4412-common.git -b android-5.1 device/samsung/smdk4412-common
+git clone https://github.com/FullgreenDEVomnirom/omnirom_device_samsung_c1skt-common.git -b android-6.0 device/samsung/c1skt-common
 fi
 
 if [ -d kernel/samsung/smdk4412 ]; then 
@@ -666,7 +669,7 @@ else
 tput setaf 2
 echo "kernel/samsung/smdk4412[DOWNLOAD]"
 tput setaf 3
-git clone https://github.com/FullGreen/omnirom_kernel_samsung_smdk4412.git -b android-5.1 kernel/samsung/smdk4412
+git clone https://github.com/FullgreenDEVomnirom/omnirom_kernel_samsung_smdk4412.git -b android-6.0 kernel/samsung/smdk4412
 fi
 
 if [ -d vendor/samsung ]; then 
@@ -678,7 +681,7 @@ else
 tput setaf 2
 echo "vendor/samsung[DOWNLOAD]"
 tput setaf 3
-git clone https://github.com/FullGreen/omnirom_proprietary_vendor_samsung.git -b android-5.1 vendor/samsung
+git clone https://github.com/FullgreenDEVomnirom/proprietary_vendor_samsung.git -b android-6.0 vendor/samsung
 fi
 
 if [ -d packages/apps/SamsungServiceMode ]; then 
@@ -690,7 +693,7 @@ else
 tput setaf 2
 echo "packages/apps/SamsungServiceMode[DOWNLOAD]"
 tput setaf 3
-git clone https://github.com/omnirom/android_packages_apps_SamsungServiceMode.git -b android-5.1 packages/apps/SamsungServiceMode
+git clone https://github.com/omnirom/android_packages_apps_SamsungServiceMode.git -b android-6.0 packages/apps/SamsungServiceMode
 fi
 
 if [ -d frameworks/opt/telephony ]; then 
@@ -700,12 +703,12 @@ rm -Rf frameworks/opt/telephony
 tput setaf 2
 echo "frameworks/opt/telephony[DOWNLOAD]"
 tput setaf 3
-git clone https://github.com/Fullgreen/omnirom_frameworks_opt_telephony.git -b android-5.1 frameworks/opt/telephony
+git clone https://github.com/FullgreenDEVomnirom/android_frameworks_opt_telephony.git -b android-6.0 frameworks/opt/telephony
 else
 tput setaf 2
 echo "frameworks/opt/telephony[DOWNLOAD]"
 tput setaf 3
-git clone https://github.com/Fullgreen/omnirom_frameworks_opt_telephony.git -b android-5.1 frameworks/opt/telephony
+git clone https://github.com/FullgreenDEVomnirom/android_frameworks_opt_telephony.git -b android-6.0 frameworks/opt/telephony
 fi
 
 if [ -d hardware/samsung ]; then 
@@ -715,12 +718,12 @@ rm -Rf hardware/samsung
 tput setaf 2
 echo "hardware/samsung[DOWNLOAD]"
 tput setaf 3
-git clone https://github.com/Fullgreen/android_hardware_samsung.git -b android-5.1 hardware/samsung
+git clone https://github.com/FullgreenDEVomnirom/android_hardware_samsung.git -b android-6.0 hardware/samsung
 else
 tput setaf 2
 echo "hardware/samsung[DOWNLOAD]"
 tput setaf 3
-git clone https://github.com/Fullgreen/android_hardware_samsung.git -b android-5.1 hardware/samsung
+git clone https://github.com/FullgreenDEVomnirom/android_hardware_samsung.git -b android-6.0 hardware/samsung
 fi
 ;;
 
@@ -754,7 +757,7 @@ ha)
 echo "ro.fullgreen.rom=haxynox" >> device/samsung/c1skt/system.prop
 ;;
 om)
-echo "ro.fullgreen.rom=omnirom" >> device/samsung/i9300/system.prop
+echo "ro.fullgreen.rom=omnirom" >> device/samsung/c1skt/system.prop
 ;;
 te)
 echo "ro.fullgreen.rom=temasek" >> device/samsung/c1skt/system.prop
@@ -807,6 +810,6 @@ ha)
 om)
 		clear
 		. build/envsetup.sh
-       brunch i9300
+       brunch c1skt
 ;;
 esac
